57950f90dd9f809af4abd8e28dc58e2136dde95e
Pavel Nedosika <nedosika@gmail.com>
2022-05-26 22:50:56 +0300

    add definePlugin and add options to the css minimizer plugin (#12)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Babel from "Components/Babel";
 import Webpack from "Components/Webpack";
 
 const App = () => {
+    console.log('Running App version ' + VERSION);
+    console.log(process.env)
     return (
         <div>
             <h1>Hello from React</h1>
