c954211f719e245e4517819ea7d17b72441ad5ba
nedosika <nedosika@gmail.com>
2022-05-26 15:22:19 +0300

    add image loader

---
+++ src/components/Babel/Babel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import babelImage from "./babel.png";
+
+const Babel = () => {
+    return (
+        <img alt="babel image" src={babelImage}/>
+    );
+};
+
+export default Babel;
---
+++ src/components/Webpack/Webpack.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import webpackImage from "./webpack.png";
+
+const Webpack = () => {
+    return (
+        <img alt="webpack image" src={webpackImage}/>
+    );
+};
+
+export default Webpack;
