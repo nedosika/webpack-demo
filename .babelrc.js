e6f09474483b4b19ec3db752e5635f56c4e1235b
Pavel Nedosika <nedosika@gmail.com>
2022-05-26 11:52:05 +0300

    Revert "Feature 118 webpack"

--- .babelrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // can be use this presets and plugins in webpack.config.js file near loader
-    presets: ["@babel/preset-env", "@babel/preset-react"],
-    plugins: ["@babel/plugin-transform-runtime"]
-};
