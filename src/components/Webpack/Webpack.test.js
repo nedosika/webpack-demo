5cc80477a992fa016f03152bd3a6a7342556651a
nedosika <nedosika@gmail.com>
2022-05-27 23:38:49 +0300

    add mui5 and create tests

--- src/components/Webpack/Webpack.test.js
+++ src/components/Webpack/Webpack.test.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {render, fireEvent, screen} from '@testing-library/react'
-import Webpack from '../Webpack';
+import Webpack from 'Components/Webpack';
 
 test('test button', () => {
     render(<Webpack/>);
--- src/components/Webpack/index.js
+++ src/components/Webpack/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import webpackImage from "./webpack.png";
 import styles from "./styles.module.css";
+import CircularIndeterminate from "../CircularIndeterminate";
 
 const Webpack = () => {
     return (
         <div>
+            <CircularIndeterminate/>
             <img className={styles.img} alt="webpack image" src={webpackImage}/>
-            <input type='text'/>
+            <input className={styles.input} type='text'/>
             <button>test</button>
         </div>
     );
