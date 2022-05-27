a1703fd5add33d24d96b0699584274f8c2314b44
Pavel Nedosika <nedosika@gmail.com>
2022-05-27 23:44:15 +0300

    [#15] add mui (#16)
    * add react testing library
    
    * add babel, css and file transformers
    
    * add mui5 and create tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-    moduleDirectories: [
-        'node_modules',
-        // +   // add the directory with the test-utils.js file, for example:
-        //     +   'utils', // a utility folder
-        // +    __dirname, // the root directory
-    ],
-    // ... other options ...
     setupFilesAfterEnv: [
         "@testing-library/jest-dom/extend-expect",
-        '<rootDir>/setUpTests.js'
+        '<rootDir>/src/setupTests.js'
     ],
     testEnvironment: 'jsdom',
     transform: {
@@ -16,4 +9,5 @@ module.exports = {
         "^.+\\.css$": "jest-transform-css",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js"
     },
+    modulePaths: ['<rootDir>/src'],
 }
