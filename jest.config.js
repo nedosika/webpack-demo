679908db1759dd1c2c1654858c61198144e4add8
nedosika <nedosika@gmail.com>
2022-05-27 13:29:44 +0300

    add babel, css and file transformers

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,14 @@ module.exports = {
         // +    __dirname, // the root directory
     ],
     // ... other options ...
-    //setupFilesAfterEnv: ['<rootDir>/setUpTests.js'],
-    "setupFilesAfterEnv": [ "@testing-library/jest-dom/extend-expect" ],
-    testEnvironment: 'jsdom'
+    setupFilesAfterEnv: [
+        "@testing-library/jest-dom/extend-expect",
+        '<rootDir>/setUpTests.js'
+    ],
+    testEnvironment: 'jsdom',
+    transform: {
+        "^.+\\.(js|ts)$": "babel-jest",
+        "^.+\\.css$": "jest-transform-css",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js"
+    },
 }
--- src/components/Webpack/index.js
+++ src/components/Webpack/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-//import webpackImage from "./webpack.png";
-//import styles from "./styles.module.css";
+import webpackImage from "./webpack.png";
+import styles from "./styles.module.css";
 
 const Webpack = () => {
     return (
         <div>
-            {/*<img className={styles.img} alt="webpack image" src={webpackImage}/>*/}
+            <img className={styles.img} alt="webpack image" src={webpackImage}/>
             <input type='text'/>
             <button>test</button>
         </div>
