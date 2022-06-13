ac2530248669c35bc171139384ec504009ef7216
Pavel Nedosika <nedosika@gmail.com>
2022-06-14 00:50:49 +0300

    [#118] create carousel (#26)
    * create carousel
    
    * create carousel

---
+++ src/components/Content/Carousel/Carousel.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from "./Carousel.module.scss";
+import earlyBooking from "./assets/img/early-booking-banner.jpg";
+
+const Carousel = () => {
+    return (
+        <section className={styles.root}>
+            <div className={styles.wrapper}>
+                <ul className={styles.indicators}>
+                    <li/>
+                    <li className={styles.active}/>
+                    <li/>
+                    <li/>
+                    <li/>
+                    <li/>
+                </ul>
+
+                <div className={styles.content}>
+                    <div className={styles.item}>
+                        <div className={styles.area}>
+                            <img
+                                title="Раннее бронированиеавтомобиля"
+                                alt="Раннее бронированиеавтомобиля"
+                                className={styles.image}
+                                src={earlyBooking}
+                            />
+                            <div className={styles.info}>
+                                <div className={styles.title}><span>Раннее бронирование</span>автомобиля</div>
+                                <div className={styles.text}>
+                                    Забронируйте понравившееся авто заранее и получите скидку до 15%
+                                </div>
+                                <a href="#">
+                                    <span className={styles.btn}>Подробнее</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Carousel;
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Order from "Components/Content/Order";
 import styles from "./Content.module.scss";
 import Cars from "Components/Content/Cars";
+import Carousel from "Components/Content/Carousel";
 
 const Content = () => {
     return (
         <main className={styles.root}>
             <Order/>
             <Cars/>
+            <Carousel/>
         </main>
     );
 };
